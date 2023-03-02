d0db49e95817820e25882bdeeab90f4b05385509
luksvrd <ljvy9b@gmail.com>
2023-03-01 23:10:29 -0600

    add thoughts & reactions route.js, wire up server

--- app/server.js
+++ app/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import reactionRoutes from "./reaction/routes.js";
+import thoughtRoutes from "./thought/routes.js";
 import userRouter from "./user/routes.js";
 
 const PORT = 3000;
@@ -7,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use("/users", userRouter);
+app.use("/thoughts", thoughtRoutes);
+app.use("/reactions", reactionRoutes);
 
 export default () => {
   app.listen(PORT, () => {
